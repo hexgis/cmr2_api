4a2e5688c1a9c1538865f4a62cee1d6e8752e333
caiocacastro <caiocacastro@gmail.com>
2022-01-14 13:42:16 -0300

    Addind centroid to priority serializer.

--- priority_monitoring/serializers.py
+++ priority_monitoring/serializers.py
@@ -5,18 +5,19 @@ from rest_framework_gis import serializers as gis_serializers
 from priority_monitoring import models
 
 
-class PriorityConsolidatedSerializer(
-    gis_serializers.GeoFeatureModelSerializer
-):
+class PriorityConsolidatedSerializer(gis_serializers.GeoFeatureModelSerializer):
     """Serializer for `models.PriorityConsolidated` data."""
+
     class Meta:
         model = models.PriorityConsolidated
-        geo_field = 'geom'
+        geo_field = "geom"
         id_field = False
         fields = (
-            'id',
-            'nu_area_km2',
-            'prioridade',
+            "id",
+            "nu_area_km2",
+            "prioridade",
+            "nu_latitude",
+            "nu_longitude",
         )
 
 
@@ -27,25 +28,25 @@ class PriorityConsolidatedDetailSerializer(serializers.ModelSerializer):
         model = models.PriorityConsolidated
         id_field = False
         fields = (
-            'no_estagio',
-            'no_image',
-            'dt_image',
-            'nu_orbita',
-            'nu_ponto',
-            'dt_t0',
-            'dt_t1',
-            'nu_area_km2',
-            'nu_area_ha',
-            'nu_latitude',
-            'nu_longitude',
-            'tempo',
-            'contribuicao',
-            'velocidade',
-            'contiguidade',
-            'ranking',
-            'prioridade',
-            'co_cr',
-            'no_cr',
-            'co_funai',
-            'no_ti',
+            "no_estagio",
+            "no_image",
+            "dt_image",
+            "nu_orbita",
+            "nu_ponto",
+            "dt_t0",
+            "dt_t1",
+            "nu_area_km2",
+            "nu_area_ha",
+            "nu_latitude",
+            "nu_longitude",
+            "tempo",
+            "contribuicao",
+            "velocidade",
+            "contiguidade",
+            "ranking",
+            "prioridade",
+            "co_cr",
+            "no_cr",
+            "co_funai",
+            "no_ti",
         )
