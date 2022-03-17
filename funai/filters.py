f522a55e77325b03fb9b33be067d13d3a1048fe4
dagnaldo <naldo.penha@gmail.com>
2022-03-17 18:45:05 -0300

    Changing co_cr filtering to accept multiple data;

--- funai/filters.py
+++ funai/filters.py
@@ -3,14 +3,23 @@ from django_filters import rest_framework
 from funai import models
 
 
+class NumberInFilter(
+    rest_framework.BaseInFilter,
+    rest_framework.NumberFilter
+):
+    """Base class used for creating IN lookup filters to filter numbers."""
+    pass
+
+
 class LimiteTerraIndigenaFilter(rest_framework.FilterSet):
     """LimiteTerraIndigenaFilter data.
 
     Filters:
         co_cr (int): regional coordination code.
     """
-    co_cr = rest_framework.NumberFilter(
+    co_cr = NumberInFilter(
         field_name='co_cr',
+        lookup_expression='in',
         required=False
     )
 
