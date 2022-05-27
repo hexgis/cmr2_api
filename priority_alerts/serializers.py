0a7b7c71974b25525584fbaf9f1817a426a83980
lucasSena <smartsena@gamail.com>
2022-05-27 17:55:58 -0300

    Serializes fields

--- priority_alerts/serializers.py
+++ priority_alerts/serializers.py
@@ -13,8 +13,12 @@ class AlertsSerializers(gis_serializers.GeoFeatureModelSerializer):
         model = models.UrgentAlerts
         id_field = False
         geo_field = 'geom'
-        fields = '__all__'
-
+        fields = (
+            'id',
+            'no_estagio',
+            'nu_latitude',
+            'nu_longitude',
+        )
 
 class AlertsTableSerializers(serializers.ModelSerializer):
     """Serializer to return data without geometry from `models.UrgentAlerts` 
@@ -23,8 +27,28 @@ class AlertsTableSerializers(serializers.ModelSerializer):
     class Meta:
         """Meta class for `AlertsTableSerializers` serializer."""
         model = models.UrgentAlerts
-        fields = '__all__'
-
+        fields = (
+            'id',
+            'no_ciclo',
+            'no_titulo',
+            'no_arquivo',
+            'nu_referencia',
+            'nu_mapa',
+            'no_estagio',
+            'no_imagem',
+            'nu_orbita_ponto',
+            'dt_t_zero',
+            'dt_t_um',
+            'nu_area_ha',
+            'co_funai',
+            'no_ti',
+            'co_cr',
+            'ds_cr',
+            'no_municipio',
+            'sg_uf',
+            'nu_longitude',
+            'nu_latitude',
+        )
 
 class AlertsClassesSerializers(serializers.ModelSerializer):
     """Serializer to list classification stages adopted in mapping the 
@@ -34,4 +58,6 @@ class AlertsClassesSerializers(serializers.ModelSerializer):
     class Meta:
         """Meta class for `AlertsClassesSerializers` serializer."""
         model = models.UrgentAlerts
-        fields = '__all__'
+        fields = (
+            'no_estagio',
+        )
