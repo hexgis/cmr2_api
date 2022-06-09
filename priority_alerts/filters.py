624f3fcdaf517cb20c6a543c69685bbf0f3ca5b3
lucasSena <smartsena@gamail.com>
2022-06-09 19:14:30 -0300

    Adding new filters to APP priority_alerts

--- priority_alerts/filters.py
+++ priority_alerts/filters.py
@@ -2,19 +2,50 @@ from django_filters import rest_framework
 
 from priority_alerts import models
 
+class NumberInFilter(
+    rest_framework.BaseInFilter,
+    rest_framework.NumberFilter
+):
+    """Base class used for creating IN lookup filters to filter numbers."""
+    pass
 
 class AlertsFilter(rest_framework.FilterSet):
     """Django filter `models.UrgentAlerts` data.
 
     Filter:
-        * co_funai (int): filtering Indigenous Lands using Funai code.
+        * co_cr (list): filtering Regional Coordination using code.
+        * co_funai (list): filtering Indigenou Lands using Funai code.
+        * start_date (str): filtering start date.
+        * end_date (str): filteringend ende date.
     """
-    co_funai = rest_framework.NumberFilter(
+    co_funai = NumberInFilter(
         field_name='co_funai',
-        lookup_expr='exact'
+        lookup_expr='in'
+    )
+
+    co_cr = NumberInFilter(
+        field_name='co_cr',
+        lookup_expr='in'
+    )
+
+    start_date = rest_framework.DateFilter(
+        field_name='dt_t_um',
+        lookup_expr='gte',
+        required=False
+    )
+
+    end_date = rest_framework.DateFilter(
+        field_name='dt_t_um',
+        lookup_expr='lte',
+        required=False
     )
 
     class Meta:
         """Meta class for `AlertsFilter` filter."""
         model = models.UrgentAlerts
-        fields = ('co_funai',)
+        fields = (
+            'co_funai',
+            'co_cr',
+            'start_date',
+            'end_date',
+        )
