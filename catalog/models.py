011ee1f22aa1d471ab436151b4d890991d862cf2
lucasSena <smartsena@gamail.com>
2022-08-29 20:18:27 -0300

    Create Sattelite model

---
+++ catalog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from django.utils.translation import ugettext_lazy as _
+
+class Sattelite (models.Model):
+    """Model to store satellite information."""
+
+    identifier = models.CharField(
+        _("Identifier"),
+        max_length=40,
+        unique=True,
+    )
+
+    name = models.CharField(
+        _("Name sattelite"),
+        max_length=255,
+        blank=True, # blank=False,
+        null=True, # null=False,
+    )
+
+    description = models.TextField(
+        _("Description"),
+        max_length=510,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name or self.identifier
