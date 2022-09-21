da2e63cdac8db195b58bebf1e4a79add2c54189a
lucas_alves <lucas_alves>
2022-09-21 19:02:19 -0300

    Requested action

--- documental/views.py
+++ documental/views.py
@@ -22,7 +22,7 @@ class AuthModelMix:
 class ActionListView(AuthModelMix, generics.ListAPIView):
     """Returns the list of data in `models.DocsAction`."""
 
-    queryset = models.DocsAction.objects.all().order_by('no_action')
+    queryset = models.DocsAction.objects.all()
     serializer_class = serializers.ActionListSerializers
     filterset_class = documental_filters.DocsActionFilter
 
@@ -38,8 +38,24 @@ class DocumentalListViews(AuthModelMix, generics.ListAPIView):
         * map_year (list): filteringend years of the maps.
     """
 
-    filterset_class = documental_filters.DocumentalDocsFilter
     filter_backends = (DjangoFilterBackend,)
+    filterset_class = documental_filters.DocumentalDocsFilter
+
+    def get_action_type(self):
+        """Função get_action_type"""
+
+        requested_action = self.request.GET.get('id_acao')
+        requested_action = list(map(int, requested_action.split(',')))
+
+        action_type_docs = models.DocsAction.objects.values(
+            'action_type').filter(id_action__in=requested_action).distinct()
+
+        if action_type_docs.count() == 1:
+            return str(action_type_docs[0]['action_type'])
+        else:
+            raise exceptions.ParseError(
+                "Não permitido retorno de dados com mais de uma TIPO DE AÇÃO"
+                " na mesma requisição ou TIPO DE AÇÃO não registrada", None)
 
     def get_serializer_class(self):
         """Get method to return data acoording to the action category selected
@@ -51,14 +67,12 @@ class DocumentalListViews(AuthModelMix, generics.ListAPIView):
             `serializers.MapasUsoOcupacaoSoloSerializers` or
             `serializers.DocumentosTISerializers`.
         """
-        
+
         action_type_docs = self.get_action_type()
-        
+
         if action_type_docs == "DOCUMENTS_TI":
-            # self.filterset_class = documental_filters.DocsDocumentTIFilter
             return serializers.DocsDocumentTISerializers
         elif action_type_docs == "MAPS_TI":
-            # self.filterset_class = documental_filters.DocsLandUserFilter
             return serializers.DocsLandUserSerializers
         elif action_type_docs == "MAPOTECA":
             return serializers.DocsMapotecaSerializers
@@ -69,28 +83,14 @@ class DocumentalListViews(AuthModelMix, generics.ListAPIView):
         action_type_docs = self.get_action_type()
 
         if action_type_docs == "DOCUMENTS_TI":
-            return models.DocsDocumentTI.objects.all().order_by('dt_registration')
+            self.filterset_class = documental_filters.DocsDocumentTIFilter
+            return models.DocsDocumentTI.objects.all()
         elif action_type_docs == "MAPS_TI":
-            return models.DocsLandUser.objects.all().order_by('dt_registration')
+            self.filterset_class = documental_filters.DocsLandUserFilter
+            return models.DocsLandUser.objects.all()
         elif action_type_docs == "MAPOTECA":
-            return models.DocsMapoteca.objects.all().order_by('dt_registration')
+            return models.DocsMapoteca.objects.all()
         else:
             raise exceptions.ParseError(
                 "XPTO Não permitido retorno de dados com mais de uma TIPO DE AÇÃO"
                 " na mesma requisição", None)
-
-    def get_action_type(self):
-        """Função get_action_type"""
-
-        requested_action = self.request.GET.get('id_acao')
-        requested_action = list(map(int, requested_action.split(',')))
-        
-        action_type_docs = models.DocsAction.objects.values(
-            'action_type').filter(id_action__in=requested_action).distinct()
-        
-        if action_type_docs.count() == 1:
-            return str(action_type_docs[0]['action_type'])
-        else:
-            raise exceptions.ParseError(
-                "Não permitido retorno de dados com mais de uma TIPO DE AÇÃO"
-                " na mesma requisição ou TIPO DE AÇÃO não registrada", None)
