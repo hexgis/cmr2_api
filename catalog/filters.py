a88602b25f3ee1ee1b766d20bd1c39c7e7647c4c
LucasSena <smartsena@gmail.com>
2022-11-28 15:57:52 -0300

    Removed unused lines code

--- catalog/filters.py
+++ catalog/filters.py
@@ -2,6 +2,7 @@ from django_filters import rest_framework
 
 from catalog import models
 
+
 class CharInFilter(
     rest_framework.BaseInFilter,
     rest_framework.CharFilter
@@ -10,8 +11,8 @@ class CharInFilter(
     pass
 
 
-class CatalogFilter(rest_framework.FilterSet):
-    """Django filter `models.Catalog` data.
+class CatalogsFilter(rest_framework.FilterSet):
+    """Django filter `models.Catalogs` data.
 
     Filters:
         * satellite (list_str): filtering Satellite using identify.
@@ -41,52 +42,11 @@ class CatalogFilter(rest_framework.FilterSet):
     )
 
     class Meta:
-        """Meta class for 'CatalogFilter' filter."""
-        model = models.Catalog
+        """Meta class for 'CatalogsFilter' filter."""
+        model = models.Catalogs
         fields = (
             'satellite',
             'cloud_cover',
             'start_date',
             'end_date',
         )
-
-
-# class CatalogsFilter(rest_framework.FilterSet):
-#     """Django filter `models.Catalog` data.
-
-#     Filters:
-#         * satellite (list): filtering Satellite using identify.
-#         * cloud_cover (list): filtering less than or equal for cloud values.
-#         * start_date (str): filtering start date.
-#         * end_date (str): filtering end date.
-#     """
-#     satellite = rest_framework.CharFilter(
-#         field_name='satellite_id__identifier',
-#         lookup_expr='exact',
-#         required=True,
-#     )
-
-#     cloud_cover = rest_framework.NumberFilter(
-#         field_name='cloud_cover',
-#         lookup_expr='lte',
-#     )
-
-#     start_date = rest_framework.DateFilter(
-#         field_name='date',
-#         lookup_expr='gte',
-#     )
-
-#     end_date = rest_framework.DateFilter(
-#         field_name='date',
-#         lookup_expr='lte',
-#     )
-
-#     class Meta:
-#         """Meta class for 'CatalogFilter' filter."""
-#         model = models.Catalogs
-#         fields = (
-#             # 'satellite',
-#             'cloud_cover',
-#             'start_date',
-#             'end_date',
-#         )
--- catalog/serializers.py
+++ catalog/serializers.py
@@ -5,6 +5,7 @@ from catalog import models
 
 class SatelliteSerializer(serializers.ModelSerializer):
     """Serializer to return registered sattelites `models.Satellite` data."""
+    # satellite = SatteliteSerializer()
 
     class Meta:
         """Meta Class for `catalog.SatteliteSerializer` serializer."""
@@ -15,26 +16,7 @@ class SatelliteSerializer(serializers.ModelSerializer):
         )
 
 
-class CatalogSerializer(serializers.ModelSerializer):
+class CatalogsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Catalog
+        model = models.Catalogs
         fields = "__all__"
-
-
-# class Landsat8CatalogSerializer(serializers.ModelSerializer):
-#     """Serializer to return `models.Landsat8Catalog` data."""
-#     # satellite = SatteliteSerializer()
-#     class Meta:
-#         """Meta Class for `catalog.Landsat8CatalogSerializer` serializer."""
-#         model = models.Landsat8Catalog
-#         fields = "__all__"
-
-
-# class Sentinel2CatalogSerializer(serializers.ModelSerializer):
-#     """Serializer to return `models.Sentinel2Catalog` data."""
-#     # satellite = SatteliteSerializer()
-
-#     class Meta:
-#         """Meta Class for `catalog.Sentinel2CatalogSerializer` serializer."""
-#         model = models.Sentinel2Catalog
-#         fields = "__all__"
