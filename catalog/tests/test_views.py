12d25c734a7f37bdcd411708f77785c6143f9386
lucasSena <smartsena@gamail.com>
2022-12-08 19:39:42 -0300

    Test views

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,119 +1,53 @@
+# python manage.py test catalog.tests.test_models
+from urllib import response
 from django.test import TestCase
+
+from django.core.management import call_command
 
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
+
+from django.test import Client
 
 
-class YourTestClass(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
-
-    def setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
-
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
-
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
-
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1, 2)
-
-
-class CatalogViewsTests(TestCase):
+class CatalogViewsURLsTests(APITestCase):
     """ Test case MonitoringFiltersTestCase data."""
 
-
     @classmethod
-    def setUpClass(cls):
-        """SetUp for class."""
-        # super(CatalogViewsTests, cls).setUpClass()
-        cls.client = APIClient
-        # cls.url_base = reverse('')
+    def setUpTestData(cls):
+        call_command('loaddata', 'catalog/fixtures/catalog_satellites.yaml', verbosity=0)
+        call_command('loaddata', 'catalog/fixtures/catalog_scnes.yaml', verbosity=0)
+        cls.url_catalogs = reverse('catalog:catalog-scenes')
         cls.url_satellite = reverse('catalog:satellite-catalog')
-        # cls.url_catalogs = reverse('catalog:')
 
     def setUp(self):
         """SetUp for methods."""
-    
-    def tearDown(self):
-        """TearDown for methods."""
-        pass    
-    # @classmethod
-    # def tearDownClass(cls):
-    #     pass
-    def test_catalogs_request(self):
+        self.client = Client()
+ 
+    def test_xpto1(self):
         response = self.client.get(self.url_satellite)
+        # self.assertEqual(response.status_code, 200)
+        print(response.json()[0])
+        # import pdb; pdb.set_trace()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
+    def test_url_catalogs(self):
+        response = self.client.get(self.url_catalogs)
+        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     
-    # @classmethod
-    # def setUpClass(cls):
-    #     """SetUp for Class."""
-    #     cls.client = APIClient()
-    #     cls.url_base = reverse('monitoring:monitoring')
-    #     cls.url_detail = reverse('monitoring-detail')
-    #     cls.url_map_stats = reverse('monitoring:monitoring-map-stats')
-    #     cls.url_classes = reverse('monitoring:monitoring-classes')
-    #     cls.url_table = reverse('monitoring:monitoring-table')
-    #     cls.url_table_stats = reverse('monitoring:monitoring-table-stats')
 
-    # def setUp(self):
-    #     """SetUp for methods."""
-
-    # def tearDown(self):
-    #     """TearDown for methods."""
-    #     pass
-
-    # def test_if_filters_is_correct(self):
-    #     home_url = reverse('monitoring:monitoring')
-    #     self.assertEqual(home_url, '/')
-
-
-
-
-
-
-
-    # def url_source(self):
-    #     # Make request
-    #     response = self.client.get(self.url_base)
-    #     self.assertEqual(self.url_map_stats, '/')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_map_stats(self):
-    #     # Make request
-    #     response = self.client.get(self.url_map_stats)
-
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_classes(self):
-    #     # Make request
-    #     response = self.client.get(self.url_classes)
-
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_table(self):
-    #     # Make request
-    #     response = self.client.get(self.url_table)
-
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_table_stats(self):
-    #     # Make request
-    #     response = self.client.get(self.url_table_stats)
-
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # @classmethod
-    # def tearDownClass(cls):
-    #     """TearDown for Class."""
-    #     pass
+class CatalogFiltersViewsTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        call_command('loaddata', 'catalog/fixtures/catalog_satellites.yaml', verbosity=0)
+        call_command('loaddata', 'catalog/fixtures/catalog_scnes.yaml', verbosity=0)
+        cls.url_catalogs = reverse('catalog:catalog-scenes')
+        cls.url_satellite = reverse('catalog:satellite-catalog')
+        
+    def test_xpto1(self):
+        print("------------>>>>>>>>>>-------------")
+        self.client.get(self.url_catalogs, {'satellite': 'LC08', 'cloud_cover': 5})
+        self.assertEqual(1 + 1, 2)
