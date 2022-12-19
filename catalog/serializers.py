f57ffa9cb2d5f9c8483afb6eca19962b56a2aeb6
lucasSena <smartsena@gamail.com>
2022-12-19 18:28:47 -0300

    Adjastements necessary to Front End

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 from catalog import models
 
@@ -16,7 +17,32 @@ class SatelliteSerializer(serializers.ModelSerializer):
         )
 
 
-class CatalogsSerializer(serializers.ModelSerializer):
+class CatalogsSerializer(GeoFeatureModelSerializer):
+    
+    count = serializers.SerializerMethodField()
+
+    def get_count(self, obj):
+    #"""Atributo temporário até se implentado a paginação neste APP"""
+
+       return getattr(obj, 'countt', 20)
+
     class Meta:
+
         model = models.Catalogs
-        fields = "__all__"
+        geo_field = 'geom'
+        fields = (
+            'geom',
+            "objectid",
+            "image",
+            "type",
+            "image_path",
+            "url_tms",
+            "date",
+            "pr_date",
+            "cloud_cover",
+            "sat",
+            "preview",
+            "count",
+            "max_native_zoom",
+            "locator",
+        )
