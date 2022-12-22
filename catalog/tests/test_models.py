31f9c3ae2c08feb3f8036aa9a8ab3dc93c666775
lucasSena <smartsena@gamail.com>
2022-12-22 19:44:27 -0300

    Testes

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -1,26 +1,30 @@
-# python manage.py test catalog.tests.test_models
 from django.test import TestCase
 
 from catalog import models
-from django.core.management import call_command 
+from django.core.management import call_command
 
 
 class CatalogsTest(TestCase):
+    """"""
     @classmethod
     def setUpTestData(cls):
+        """"""
         call_command('loaddata', 'catalog/fixtures/catalog_satellites.yaml', verbosity=0)
         call_command('loaddata', 'catalog/fixtures/catalog_scnes.yaml', verbosity=0)
 
     def test_objects_created(self):
+        """"""
         self.assertTrue(models.Catalogs.objects.exists())
 
     def test_object_name_is_image(self):
+        """"""
         catalog_image = models.Catalogs.objects.first()
         expected_object_name = f'{catalog_image.image}'
 
         self.assertEqual(expected_object_name, str(catalog_image))
     
     def test_related_catalog_satellite_exists(self):
+        """"""
         catalog_image = models.Catalogs.objects.values_list('sat', 'sat_id__name')[:1].get()
         satellite = models.Satellite.objects.get(id=catalog_image[0])
 
@@ -28,15 +32,18 @@ class CatalogsTest(TestCase):
 
 
 class SatelliteTest(TestCase):
+    """"""
 
     @classmethod
     def setUpTestData(cls):
         call_command('loaddata', 'catalog/fixtures/catalog_satellites.yaml', verbosity=0)
 
-    def ttest_object_created(self):
-        self.assertTrue(models.Satellite.objects.exists())
+    def test_object_created(self):
+        """"""
+        self.assertFalse(models.Satellite.objects.exists())
 
     def test_object_model_exit_is_name_or_identifier(self):
+        """"""
         satellite = models.Satellite.objects.first()
         expected_object_name = [satellite.name, satellite.identifier]
 
--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -9,6 +9,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from django.test import Client
+from catalog import models
 
 
 class CatalogViewsURLsTests(APITestCase):
@@ -25,10 +26,10 @@ class CatalogViewsURLsTests(APITestCase):
         """SetUp for methods."""
         self.client = Client()
  
-    def test_xpto1(self):
+    def test_url_satellite(self):
         response = self.client.get(self.url_satellite)
         # self.assertEqual(response.status_code, 200)
-        print(response.json()[0])
+        # print(response.json()[0], "Primeira class")
         # import pdb; pdb.set_trace()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -46,8 +47,20 @@ class CatalogFiltersViewsTests(APITestCase):
         call_command('loaddata', 'catalog/fixtures/catalog_scnes.yaml', verbosity=0)
         cls.url_catalogs = reverse('catalog:catalog-scenes')
         cls.url_satellite = reverse('catalog:satellite-catalog')
-        
+
+        request_ok = self.client.get(self.url_catalogs, {'satellite': 'LC08', 'cloud_cover': 99, 'start_date':'2018-06-14','end_date':'2021-10-14'})#; len(xxx.json())
+        # queryset_ok = models.Catalogs.objects.filter(sat__exact=2, cloud_cover=99, date__gte = '2015-06-14', date__lte = '2022-06-14'))
+        # request_error = self.client.get(self.url_catalogs, {'satellite': 1, 'cloud_cover': '5'})
+
     def test_xpto1(self):
         print("------------>>>>>>>>>>-------------")
-        self.client.get(self.url_catalogs, {'satellite': 'LC08', 'cloud_cover': 5})
+        # self.client.get(self.url_catalogs, {'satellite': 'LC08', 'cloud_cover': 5})
+        self.request_ok
         self.assertEqual(1 + 1, 2)
+
+    # def for se todos for "LC08"
+    # def ver ser tem algum > do que 5
+    # def ver se data maior ou igual
+    # def ver se data menor ou igual
+
+# print("\n\n\ ------------------->>>>>>>>>>>>>>>>------------")
