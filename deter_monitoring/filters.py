3ceccea048c29bf6c09930336eb930f559a84ba9
LucasSena <smartsena@gmail.com>
2023-01-30 15:49:04 -0300

    Filters APP

--- deter_monitoring/filters.py
+++ deter_monitoring/filters.py
@@ -3,12 +3,12 @@ from django_filters import rest_framework
 from deter_monitoring import models
 
 
-# class NumberInFilter(
-#     rest_framework.BaseInFilter,
-#     rest_framework.NumberFilter
-# ):
-#     """Base class used for creating IN lookup filters to filter numbers."""
-#     pass
+class NumberInFilter(
+    rest_framework.BaseInFilter,
+    rest_framework.NumberFilter
+):
+    """Base class used for creating IN lookup filters to filter numbers."""
+    pass
 
 
 class CharInFilter(
@@ -23,24 +23,38 @@ class DeterTIFilter(rest_framework.FilterSet):
     """DeterTIFilter data.
 
     Fielters:
+        * co_cr (list): filtering Regional Coordenation using code.
+        * co_funai (list): filtering Indigenou Lands using Funai code
         * class_name (list_str): Classification classes. E.g.: ????
         * satellite (list_str): filtering Satellite using identify.
         * start_date (str): filtering start date.
         * end_date (str): filtering end date.
     """
+    co_cr = NumberInFilter(
+        field_name='co_cr',
+        lookup_expr='in'
+    )
+
+    co_funai = NumberInFilter(
+        field_name='co_funai',
+        lookup_expr='in'
+    )
 
     class_name = CharInFilter(
         field_name='classname',
         lookup_expr='in',
     )
+
     satellite = CharInFilter(
         field_name='satellite',
         lookup_expr='in',
     )
+
     start_date = rest_framework.DateFilter(
         field_name='view_date',
         lookup_expr='gte',
     )
+
     end_date = rest_framework.DateFilter(
         field_name='view_date',
         lookup_expr='lte',
@@ -50,6 +64,8 @@ class DeterTIFilter(rest_framework.FilterSet):
         """Meta class for `DeterTIFilter` filter."""
         model = models.DeterTI
         fields = (
+            'co_cr',
+            'co_funai',
             'class_name',
             'satellite',
             'start_date',
