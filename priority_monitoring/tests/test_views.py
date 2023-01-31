bef20be4fc63ea601dadcbb421965a4aba2176d1
lucasSena <smartsena@gamail.com>
2023-01-31 15:52:48 -0300

    Test views

--- priority_monitoring/tests/test_views.py
+++ priority_monitoring/tests/test_views.py
@@ -1,3 +1,5 @@
+from inspect import Parameter
+from time import strptime
 from urllib import response
 from rest_framework.test import APITestCase
 from django.test import Client
@@ -13,77 +15,111 @@ from priority_monitoring import models
 class PiorirityConsolidatedViewsTests(APITestCase):
     """Test case PiorirityConsolidatedViewTests data."""
 
-    @classmethod
-    def setUpTestData(cls):
-        """Class variables using dumpdata fixtures file end URLs."""
-        # call_command('loaddata', 'priority_monitoring/fixtures/priority_consolidated.yaml', verbosity=0)
-        cls.url_pioririty_consolidated = reverse('priority_monitoring:priority-consolidated')
-        cls.url_pioririty_consolidated_priorities = reverse('priority_monitoring:priority-consolidated-priorities')
-        cls.url_pioririty_consolidated_detail = reverse('priority_monitoring:priority-consolidated-detail')
-        cls.url_pioririty_consolidated_stats = reverse('priority_monitoring:priority-consolidated-stats')
-        cls.url_pioririty_consolidated_table = reverse('priority_monitoring:priority-consolidated-table')
-    
-    def SetUp (self):
-        """SetUp for methods."""
-        self.client = Client()
-
-    def test_url_pioririty_consolidated(self):
-        response = self.client.get(self.url_pioririty_consolidated)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_url_pioririty_consolidated_priorities(self):
-        response = self.client.get(self.url_pioririty_consolidated_priorities)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_url_pioririty_consolidated_detail(self):
-        response = self.client.get(self.url_pioririty_consolidated_detail)
-        #add pk
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_url_pioririty_consolidated_stats(self):
-        response = self.client.get(self.url_pioririty_consolidated_stats)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_url_pioririty_consolidated_table(self):
-        response = self.client.get(self.url_pioririty_consolidated_table)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
-class PiorirityConsolidatedFiltersViewsTests(APITestCase):
-    """Test case PiorirityConsolidatedFiltersViewsTests data."""
-    
-    def get_filters_parameters(self):
-        """Creating parameters applied to filters."""
+    def get_requered_filters_parameters(self):
+        """Creating parameters requered applied to filters."""
         parameter = {
-            'co_cr': 30202001920,
-            'co_funai': 23001,
-            'stage': 'CR',
             'start_date':'2021-12-15',
             'end_date':'2021-07-28',
-            'priority': 'Baixa',
+            'pk_detail':13
         }
 
         return parameter
 
     @classmethod
     def setUpTestData(cls):
+        """Class variables using dumpdata fixtures file end URLs."""
+        cls.parameters = cls.get_requered_filters_parameters(cls)
+
+        cls.url_pioririty_consolidated = reverse(
+            'priority_monitoring:priority-consolidated'
+        )
+        cls.url_pioririty_consolidated_priorities = reverse(
+            'priority_monitoring:priority-consolidated-priorities'
+        )
+        cls.url_pioririty_consolidated_detail = reverse(
+            'priority_monitoring:priority-consolidated-detail', 
+            kwargs={'pk':cls.parameters['pk_detail']}
+        )
+        cls.url_pioririty_consolidated_stats = reverse(
+            'priority_monitoring:priority-consolidated-stats'
+        )
+        cls.url_pioririty_consolidated_table = reverse(
+            'priority_monitoring:priority-consolidated-table'
+        )
+
+    def SetUp (self):
+        """SetUp for methods."""
+        self.client = Client()
+
+    def test_url_pioririty_consolidated(self):
+        """Test url pioririty consolidated."""
+        response = self.client.get(self.url_pioririty_consolidated, {'start_date':self.parameters['start_date'],'end_date':self.parameters['end_date']})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_url_pioririty_consolidated_priorities(self):
+        """Test url pioririty consolidated priorities."""
+        response = self.client.get(self.url_pioririty_consolidated_priorities)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_url_pioririty_consolidated_detail(self):
+        """Test url pioririty consolidated detail."""
+        response = self.client.get(self.url_pioririty_consolidated_detail)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_url_pioririty_consolidated_stats(self):
+        """Test url pioririty consolidated stats."""
+        response = self.client.get(self.url_pioririty_consolidated_stats, {'start_date':self.parameters['start_date'],'end_date':self.parameters['end_date']})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_url_pioririty_consolidated_table(self):
+        """Test url pioririty consolidated table."""
+        response = self.client.get(self.url_pioririty_consolidated_table, {'start_date':self.parameters['start_date'],'end_date':self.parameters['end_date']})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class PiorirityConsolidatedFiltersViewsTests(APITestCase):
+    """Test case PiorirityConsolidatedFiltersViewsTests data."""
+
+    def get_filters_parameters(self):
+        """Creating parameters applied to filters."""
+        parameter = {
+            'co_cr': 30202001920,
+            'co_funai': 23001,
+            'stage': 'CR',
+            'end_date':'2021-12-15',
+            'start_date':'2021-07-28',
+            'priority': 'Baixa',
+            'pk_detail':13
+        }
+
+        return parameter
+
+    @classmethod
+    def setUpTestData(cls):
+        """Class variables using dumpdata fixtures file end URLs."""
+        cls.parameters = cls.get_filters_parameters(cls)
+
         call_command(
             'loaddata',
             'priority_monitoring/fixtures/priority_consolidated.yaml',
             verbosity=0)
-        cls.url_pioririty_consolidated = reverse('priority_monitoring:priority-consolidated')
-        # cls.url_pioririty_consolidated_priorities = reverse('priority_monitoring:priority-consolidated-priorities')
-        # cls.url_pioririty_consolidated_detail = reverse('priority_monitoring:priority-consolidated-detail')
-        # cls.url_pioririty_consolidated_stats = reverse('priority_monitoring:priority-consolidated-stats')
-        # cls.url_pioririty_consolidated_table = reverse('priority_monitoring:priority-consolidated-table')
-    
+
+        cls.url_pioririty_consolidated_detail = reverse(
+            'priority_monitoring:priority-consolidated-detail', 
+            kwargs={'pk':cls.parameters['pk_detail']}
+        )
+        cls.url_pioririty_consolidated_table = reverse(
+            'priority_monitoring:priority-consolidated-table'
+        )
+
     def setUp(self):
+        """SetUp for methods."""
         self.client = Client()
-        self.parameters = self.get_filters_parameters()
 
         self.queryset_valid = models.PriorityConsolidated.objects.filter(
             co_cr=self.parameters['co_cr'],
@@ -93,7 +129,7 @@ class PiorirityConsolidatedFiltersViewsTests(APITestCase):
             dt_t_um__lte=self.parameters['end_date'],
             prioridade=self.parameters['priority']
         )
-        self.request_valid = self.client.get(self.url_pioririty_consolidated, {
+        self.request_valid = self.client.get(self.url_pioririty_consolidated_table, {
             'co_cr':self.parameters['co_cr'],
             'co_funai':self.parameters['co_funai'],
             'stage':self.parameters['stage'],
@@ -101,19 +137,20 @@ class PiorirityConsolidatedFiltersViewsTests(APITestCase):
             'end_date':self.parameters['end_date'],
             'priority':self.parameters['priority']
         })
-        self.request_error = self.client.get(self.url_pioririty_consolidated, {
-            'co_cr':"Kayapó"
-            #adicionar outros
+        self.request_error = self.client.get(self.url_pioririty_consolidated_table, {
+            'co_cr':"Kayapó",
+            'start_date':self.parameters['start_date'],
+            'end_date':self.parameters['end_date']
         })
 
     def test_valid_parameters(self):
-        """Test valid URL parameters on catalog-scenes."""
+        """Test valid URL parameters."""
         response = self.request_valid
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_invalid_parameters(self):
-        """Test invalid URL parameters on catalog-scenes."""
+        """Test invalid URL parameters."""
         response = self.request_error
 
         self.assertTrue(status.is_client_error(response.status_code))
@@ -124,3 +161,29 @@ class PiorirityConsolidatedFiltersViewsTests(APITestCase):
 
         self.assertTrue(qs.exists())
 
+    def test_request_with_filters_is_valid(self):
+        """Testing if the request with filters is valid."""
+        response = self.request_valid.json()
+        date_format = "%Y-%m-%d"
+
+        for i in range(len(response)):
+            self.assertEqual(response[i]['co_cr'], self.parameters['co_cr'])
+            self.assertEqual(response[i]['co_funai'], self.parameters['co_funai'])
+            self.assertEqual(response[i]['no_estagio'], self.parameters['stage'])
+            self.assertEqual(response[i]['prioridade'], self.parameters['priority'])
+            self.assertTrue(
+                datetime.strptime(self.parameters['start_date'], date_format)<=
+                datetime.strptime(response[i]['dt_t_um'], date_format)<=
+                datetime.strptime(self.parameters['end_date'], date_format)
+            )
+
+    def test_request_filter_is_equals_len_queryset_filter(self):
+        """test record amount of request is iqual to queyrsaf."""
+        response = self.request_valid.json()
+        qs = self.queryset_valid
+
+        self.assertEqual(len(response), qs.count())
+
+    def test_inbbox_filter(self):
+        #TODO: create methodology for inbbox tests
+        pass
