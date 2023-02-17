dd83f2b610748b3725154036a8b4008e78243816
lucasSena <smartsena@gamail.com>
2023-02-17 18:29:25 -0300

    URLs image_path end  preview

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from rest_framework_gis import serializers as gis_serializers
 
+import os
+from django.conf import settings
+
 from catalog import models
 
 
@@ -18,6 +21,24 @@ class SatelliteSerializer(serializers.ModelSerializer):
 
 class CatalogsSerializer(gis_serializers.GeoFeatureModelSerializer):
     """Serializer to return Catalogs scenes as GeoJSON compatible data."""
+    preview = serializers.SerializerMethodField()
+    image_path = serializers.SerializerMethodField()
+
+    def get_preview(self, instance):
+        if instance.preview is not None:
+            return os.path.join(settings.DOMAIN_API, instance.preview)
+        else:
+            return "URL não encontrada"
+
+    def get_image_path(self, instance):
+        if instance.image_path is not None:
+            url_catalog = instance.image_path.replace('\\','/').replace(
+                "//hex-funai.hex.com/", '').replace("media/", '')
+            url_catalog = os.path.join(settings.DOMAIN_API, url_catalog)
+            return url_catalog
+        else:
+            return "URL não encontrada"
+
     class Meta:
         model = models.Catalogs
         geo_field = 'geom'
