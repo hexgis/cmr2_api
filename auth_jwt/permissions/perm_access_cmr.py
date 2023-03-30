ea481fca5f431d2f648b1626efbb73131fadcc4e
lucasSena <smartsena@gamail.com>
2023-03-30 11:49:38 -0300

    Permissions access

--- auth_jwt/permissions/perm_access_cmr.py
+++ auth_jwt/permissions/perm_access_cmr.py
@@ -1,33 +1,38 @@
 from django.contrib.contenttypes.models import ContentType
-from auth_jwt.permissions import permission_granted
-
-# from rest_framework import (
-#     generics,
-#     # permissions,
-#     )
-
-# permi = ('catalog.view_satellite')
-# permis = ('catalog.view_satellite', 'catalog.access_satellite')
-# permis = ('catalog.add_satellite', 'catalog.access_satellite')
-# CMR_Modulo_Access.user_request_permission(self.request.user)
+from django.contrib.auth.models import Permission
 
 
-class CMR_Modulo_Access():
-    cmr_catalog_modulo = ("observar_satellite", "permissao_adicional_test")
-
+class CMRModuloAccess():
+    """"""
     def app_name_exists(app_name):
+        """"""
         app_list = ContentType.objects.values_list(
             'app_label', flat=True).distinct()
         if app_name in str(app_list):
             return True
-        else:
+        else: #TODO: Add execeptions
             return False
 
+    def cmr_permissions_access(app_name):
+        """"""
+        app_access_permissions=()
+        codenames = Permission.objects.values_list("codename",flat=True).filter(
+            codename__icontains="access", 
+            content_type_id__app_label__exact=app_name)
+
+        for coodename in codenames:
+            app_access_permissions += (app_name + "." + coodename,)
+
+        return app_access_permissions
+
     def user_request_permission(request_user, app_name):
-        if CMR_Modulo_Access.app_name_exists(app_name):
-            # import pdb; pdb.set_trace()
-            # print('messageDeu bom!!!\n\n', request_user, '!\n\n', app_name)
-            # print("xpto ----->>>> ", request_user.has_perms(permission_granted.permissons_access[app_name]))
-            return request_user.has_perms(permission_granted.permissons_access[app_name])
+        """"""
+        if CMRModuloAccess.app_name_exists(app_name):
+            app_access_permissions = CMRModuloAccess.cmr_permissions_access(app_name)
+            if len(app_access_permissions) <= 1:
+                cmr_permission = request_user.has_perm(app_access_permissions)
+            else:
+                cmr_permission = request_user.has_perms(app_access_permissions)
+            return cmr_permission
         else:
-            print('messageDeu ruimmmm!!!\n\n', app_name)
+            print('Aplicativo não cadastrado: ', app_name) #TODO: Add execeptions
