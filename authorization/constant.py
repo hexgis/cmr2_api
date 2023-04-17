5b3f35d7d0591e1a89907a89d6b47d2f2c64254d
lucasSena <smartsena@gamail.com>
2023-04-17 17:09:27 -0300

    Create constants file.

---
+++ authorization/constant.py
@@ -0,0 +1,40 @@
+MODULOS_CMR = {
+    "catalog-tab": {
+    	"access":{"access_Satellite","access_scens"}, 
+    	"description":"Meu acervo de imagens"},
+    "search-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Monitoramento Diário"},
+    "layers-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Camadas de Sobreposição"},
+    "high-resolution-mosaics-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Alta Resolução e Mosaicos"},
+    "support-fire-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Risco de Fogo e Focos de Calor"},
+    "landuse-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Uso e Ocupação do Solo"},
+    "prodes-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"PRODES (INPE)"},
+    "urgent-alerts-tab": {
+    	"access":{"access_urgentalerts",}, 
+    	"description":"Alerta Urgente"},
+    "priority-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Polígonos Prioritários"},
+    "document-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Documental"},
+    "mapoteca-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Mapoteca"},
+    "deter-tab": {
+    	"access":{"access_","access_"}, 
+    	"description":"Deter"}
+}
+
+
