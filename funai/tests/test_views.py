580fc691a4b40cc99b002d593cf20e3432ffd3ab
lucasSena <smartsena@gamail.com>
2023-05-09 19:38:07 -0300

    Code improvements.

--- funai/tests/test_views.py
+++ funai/tests/test_views.py
@@ -1,9 +1,7 @@
-from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
 
 from django.core.management import call_command
-from django.contrib.auth.models import User
-from django.test import RequestFactory
 from django.urls import reverse
 
 from funai import models
@@ -16,10 +14,14 @@ class FuaniViewsTests(APITestCase):
         """Class variables APP URLs."""
         cls.url_funai_cr = reverse('funai:coordenacao-regional')
         cls.url_funai_ti = reverse('funai:terras-indigenas')
+        cls.parameter_co_cr_nonexistent = 99999999
 
     def setUp(self):
         """SetUp for methods."""
         self.client = APIClient()
+        #TODO: After adding authentication and authorization in the FUNAI APP.
+        # from django.test import RequestFactory
+        # from django.contrib.auth.models import User
         # self.client.force_authenticate(user=self.user)
 
     def test_url_coordenacao_regional(self):
@@ -39,27 +41,28 @@ class FuaniViewsTests(APITestCase):
             request.status_code, status.HTTP_200_OK, 
             'Expected Response Code 200, received {0}:{1} instead.' .format(
                 request.status_code,request.status_text))
+    
+    def test_url__terra_indigena_invalid_co_cr(self):
+        request = self.client.get(
+            self.url_funai_ti, {'co_cr':self.parameter_co_cr_nonexistent,})
+        
+        self.assertEqual(
+            request.status_code, status.HTTP_400_BAD_REQUEST,
+            'id_acao valid is requirid. Expected Response Code 400, received \
+                {0}:{1} instead.' .format(
+                    request.status_code,request.status_text))
 
-
-class TestCoordenacaoRegionalViewsTests(APITestCase):
-    """Test case TestCoordenacaoRegionalViewsTests data."""
-    def get_requered_filters_parameters(self):
-        """Creating parameters requered applied to filters."""
-        parameter = {
-            'co_cr': 30202001962,
-        }
-
-        return parameter
-
+class FuaniViewsFiltersTests(APITestCase):
+    """Test case FuaniViewsFiltersTests data."""
     @classmethod
     def setUpTestData(cls):
         """Class variables using in load data fixtures file end URLs."""
-        cls.parameters = cls.get_requered_filters_parameters(cls)
         cls.url_funai_ti = reverse('funai:terras-indigenas')
+        cls.parameter = 30202001962
 
         call_command('loaddata', 'funai/fixtures/coordenacao_regional.yaml',
             verbosity=0)
-        call_command('loaddata', 'funai/fixtures/terras_indigenas.yaml', 
+        call_command('loaddata', 'funai/fixtures/terras_indigenas.yaml',
             verbosity=0)
 
     def setUp(self):
@@ -68,9 +71,9 @@ class TestCoordenacaoRegionalViewsTests(APITestCase):
         # self.client.force_authenticate(user=self.user)
 
         self.queryset_valid = models.LimiteTerraIndigena.objects.filter(
-            co_cr_id=self.parameters['co_cr'])
+            co_cr_id=self.parameter)
         self.request_valid = self.client.get(self.url_funai_ti, {
-            'co_cr':self.parameters['co_cr'],})
+            'co_cr':self.parameter,})
 
     def test_valid_parameters(self):
         """Test valid URL parameters in request."""
