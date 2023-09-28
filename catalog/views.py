83d2c35bb023f6e2c1df5534d65275a54658d785
Joao Fonseca <joao.fonseca@hex360.com.br>
2023-09-28 16:17:47 -0300

    temporary changes to remove permissions

--- catalog/views.py
+++ catalog/views.py
@@ -18,33 +18,33 @@ from catalog import (
 
 class AuthModelMixIn:
     """Default Authentication for catalog views."""
-    permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = (permissions.IsAuthenticated,)
 
 
-class HasAcessCMR(AuthModelMixIn):
-    """Management of the first level of access to the CMR Modules."""
-    def has_perm_access_cmr(self):
-        """Validates if the logged in user has the necessary permissions to
-        access the CMR Modules in this APP."""
-        tem_permicao_cmr = CMRModuleAccess.user_request_permission(
-            self.request.user, __package__)
-        return tem_permicao_cmr
+# class HasAcessCMR(AuthModelMixIn):
+#     """Management of the first level of access to the CMR Modules."""
+#     def has_perm_access_cmr(self):
+#         """Validates if the logged in user has the necessary permissions to
+#         access the CMR Modules in this APP."""
+#         tem_permicao_cmr = CMRModuleAccess.user_request_permission(
+#             self.request.user, __package__)
+#         return tem_permicao_cmr
 
 
-class SatelliteView(HasAcessCMR, generics.ListAPIView):
+class SatelliteView(AuthModelMixIn, generics.ListAPIView):
     """Returns the list of satellites in `models.Satellite`."""
     serializer_class = serializers.SatelliteSerializer
 
     def get_queryset(self):
         """Returns the set of data according to access and permissions granted
         to the logged in user"""
-        if self.request.user.has_perm('catalog.view_satellite') and self.has_perm_access_cmr():
-            return models.Satellite.objects.all()
-        else:
-            raise exceptions.PermissionDenied
+        # if self.request.user.has_perm('catalog.view_satellite') and self.has_perm_access_cmr():
+        return models.Satellite.objects.all()
+        # else:
+        #     raise exceptions.PermissionDenied
 
 
-class SceneView(HasAcessCMR, generics.ListAPIView):
+class SceneView(AuthModelMixIn, generics.ListAPIView):
     """Returns Scene data for the requested filter.
 
     Filters:
@@ -69,7 +69,7 @@ class SceneView(HasAcessCMR, generics.ListAPIView):
     def get_queryset(self):
         """Returns the set of data according to access and permissions granted
         to the logged in user"""
-        if self.request.user.has_perms('catalog.view_Scene') and self.has_perm_access_cmr():
-            return models.Scene.objects.all().order_by('sat_identifier')
-        else:
-            raise exceptions.PermissionDenied
+        # if self.request.user.has_perms('catalog.view_Scene') and self.has_perm_access_cmr():
+        return models.Scene.objects.all().order_by('sat_identifier')
+        # else:
+        #     raise exceptions.PermissionDenied
