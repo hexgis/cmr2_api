4cd94256dd3aa1ab9299ab95146938faf9d98c36
Joao Fonseca <joao.fonseca@hex360.com.br>
2024-04-22 14:56:32 -0300

    update funai model

--- funai/models.py
+++ funai/models.py
@@ -12,6 +12,12 @@ class CoordenacaoRegional(models.Model):
         default=1
     )
 
+    no_cr = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
     ds_cr = models.CharField(
         _('Regional Coordenation name'),
         max_length=255,
@@ -117,6 +123,12 @@ class LimiteTerraIndigena(models.Model):
         null=True
     )
 
+    geom = models.MultiPolygonField(
+        srid=4674,
+        blank=True,
+        null=True
+    )
+
     no_ti = models.CharField(
         _('Name of Indigenous Lands'),
         max_length=255,
@@ -124,60 +136,145 @@ class LimiteTerraIndigena(models.Model):
         null=True
     )
 
+    no_grupo_etnico = models.CharField(
+        _("Grupo Étnico"),
+        max_length=255,
+        blank=True,
+        null=True
+    )
+
     ds_fase_ti = models.CharField(
         _('Description for TI stage'),
-        max_length=255,
+        max_length=100,
         blank=True,
         null=True
     )
 
     ds_modalidade = models.CharField(
         _('Modality'),
-        max_length=255,
+        max_length=100,
         blank=True,
-        null=True
-    )
-
-    co_cr = models.ForeignKey(
-        'funai.CoordenacaoRegional',
-        on_delete=models.DO_NOTHING,
-        related_name='cr',
         null=True
     )
 
     ds_cr = models.CharField(
         _('Description for Regional Coordination'),
-        max_length=255,
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    co_cr = models.ForeignKey(
+        CoordenacaoRegional,
+        to_field='co_cr',
+        related_name='terras_indigenas',
+        on_delete=models.DO_NOTHING,
+        db_column='co_cr',
+        db_constraint=False
+    )
+
+    no_municipio = models.TextField(
+        _('City name'),
         blank=True,
         null=True
     )
 
     sg_uf = models.CharField(
         _('State aconymn'),
-        max_length=255,
-        blank=True,
-        null=True
-    )
-
-    nu_area_ha = models.DecimalField(
-        _('Area ha'),
-        max_digits=14,
-        decimal_places=3,
-        blank=True,
-        null=True
-    )
-
-    nu_area_km = models.DecimalField(
-        _('Area km'),
-        max_digits=14,
-        decimal_places=3,
+        max_length=20,
         blank=True,
         null=True
     )
 
     st_faixa_fronteira = models.CharField(
         _('Border strip'),
-        max_length=255,
+        max_length=3,
+        blank=True,
+        null=True
+    )
+
+    dt_em_estudo = models.DateField(
+        blank=True,
+        null=True
+    )
+    ds_portaria_em_estudo = models.TextField(
+        blank=True,
+        null=True
+    )
+    dt_delimitada = models.DateField(
+        blank=True,
+        null=True
+    )
+    ds_despacho_delimitada = models.TextField(
+        blank=True,
+        null=True
+    )
+    dt_declarada = models.DateField(
+        blank=True,
+        null=True
+    )
+    ds_portaria_declarada = models.TextField(
+        blank=True,
+        null=True
+    )
+    dt_homologada = models.DateField(
+        blank=True,
+        null=True
+    )
+    ds_decreto_homologada = models.TextField(
+        blank=True,
+        null=True
+    )
+    dt_regularizada = models.DateField(
+        blank=True,
+        null=True
+    )
+    ds_matricula_regularizada = models.TextField(
+        blank=True,
+        null=True
+    )
+    ds_doc_resumo_em_estudo = models.TextField(
+        blank=True,
+        null=True
+    )
+    ds_doc_resumo_delimitada = models.TextField(
+        blank=True,
+        null=True
+    )
+    ds_doc_resumo_declarada = models.TextField(
+        blank=True,
+        null=True
+    )
+    ds_doc_resumo_homologada = models.TextField(
+        blank=True,
+        null=True
+    )
+    ds_doc_resumo_regularizada = models.TextField(
+        blank=True,
+        null=True
+    )
+    st_amazonia_legal = models.BooleanField(
+        blank=True,
+        null=True
+    )
+
+    nu_area_ha = models.DecimalField(
+        _('Area ha'),
+        max_digits=100,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+
+    nu_area_km = models.DecimalField(
+        _('Area km'),
+        max_digits=100,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+
+    dt_cadastro = models.DateTimeField(
         blank=True,
         null=True
     )
