6bb1e3e15fdf1f28a7cb4fd548912fe3ca23f7ff
João Lima <joao.fonseca@hex360.com.br>
2024-04-26 18:34:10 -0300

    cors

--- cmr2_api/settings.py
+++ cmr2_api/settings.py
@@ -185,6 +185,27 @@ MEDIA_URL = '/media/'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 # djangorestframework authentication
 # https://www.django-rest-framework.org/api-guide/authentication/
 
