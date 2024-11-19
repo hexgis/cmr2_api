eac7d5e374b79493da766975d884dec8b79a6aeb
João Vitor Lima <joao.fonseca@hex360.com.br>
2024-11-19 19:00:52 -0300

    delete test models land use

--- land_use/tests/test_models.py
+++ land_use/tests/test_models.py
@@ -5,26 +5,26 @@ from django.core.management import call_command
 from land_use import models
 
 
-class LandUseClassesTests(APITestCase):
-    """Test case `models.land_use.LandUseClasses` model.
+# class LandUseClassesTests(APITestCase):
+#     """Test case `models.land_use.LandUseClasses` model.
 
-    Args:
-        APITestCase (class 'rest_framework.test.APITestCase'): Includes a few
-            helper classes that extend Django's existing test framework.
-    """
+#     Args:
+#         APITestCase (class 'rest_framework.test.APITestCase'): Includes a few
+#             helper classes that extend Django's existing test framework.
+#     """
 
-    @classmethod
-    def setUpTestData(cls):
-        """Class variables using dumpdata fixtures file."""
+#     @classmethod
+#     def setUpTestData(cls):
+#         """Class variables using dumpdata fixtures file."""
 
-        call_command(
-            'loaddata',
-            'land_use/fixtures/land_use_classes.yaml',
-            verbosity=0)
+#         call_command(
+#             'loaddata',
+#             'land_use/fixtures/land_use_classes.yaml',
+#             verbosity=0)
 
-    def test_objects_landuseclasses_created(self):
-        """Test if object LandUseClasses is created."""
+#     def test_objects_landuseclasses_created(self):
+#         """Test if object LandUseClasses is created."""
 
-        self.assertTrue(models.LandUseClasses.objects.exists())
+#         self.assertTrue(models.LandUseClasses.objects.exists())
 
-# TODO: Create test_views tests.
+# # TODO: Create test_views tests.
