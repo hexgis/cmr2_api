fd9279c5498aa6a3135935d804f6d972ddcc9974
João Vitor Lima <joao.fonseca@hex360.com.br>
2024-11-19 18:11:58 -0300

    static url

--- cmr2_api/urls.py
+++ cmr2_api/urls.py
@@ -100,5 +100,7 @@ urlpatterns = [
          ('admin_panel.urls', 'adm-panel'),
          namespace='adm panel'),
          ),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
