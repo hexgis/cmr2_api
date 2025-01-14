2b6d280529f1c7935db07e31356574ebc3c22ca7
Valdean Júnior <valdean.junior@hex360.com.br>
2025-01-14 15:52:38 -0300

    cleaning up unused code

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from cmr2_api.mixins import AdminPermissionMixin
 from dashboard import models
 
 
--- priority_alerts/admin.py
+++ priority_alerts/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from priority_alerts import models
-from cmr2_api.mixins import AdminPermissionMixin
 
-
-class PriorityAlertsAdmin(AdminPermissionMixin,admin.ModelAdmin):
+class PriorityAlertsAdmin(admin.ModelAdmin):
     """Django administrator `model.PriorityAlerts` data."""
 
     list_display = (
