3f882068db1a9f4fcc603b1d0ce87a8f1e6b68dc
Valdean Júnior <valdean.junior@hex360.com.br>
2025-01-29 14:42:16 -0300

    implement swagger

--- cmr2_api/settings.py
+++ cmr2_api/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'drf_spectacular',
     'rest_framework',
     'django_json_widget',
     'dashboard',
@@ -216,6 +217,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 
--- cmr2_api/urls.py
+++ cmr2_api/urls.py
@@ -18,13 +18,15 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path(
         'admin/',
         admin.site.urls
     ),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('adm-panel/', include(
         ('admin_panel.urls', 'adm-panel'),
         namespace='adm panel'),
