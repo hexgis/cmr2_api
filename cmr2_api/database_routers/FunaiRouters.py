5f7665a30539f712c1d4b534ffbe54c7665fa3d6
Joao Fonseca <joao.fonseca@hex360.com.br>
2025-05-29 14:34:04 -0300

    feat: :zap: creating fixtures for cr and ti
    Implement structural updates and optimizations across multiple modules

--- cmr2_api/database_routers/FunaiRouters.py
+++ cmr2_api/database_routers/FunaiRouters.py
@@ -5,16 +5,15 @@ class FunaiRouter:
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label in self.route_app_labels:
-            if model.__name__ == "CoordenacaoRegional":
+            if model.__name__ in ["CoordenacaoRegional", "LimiteTerraIndigena"]:
                 return 'default'
             return 'db_for_read'
         return None
 
     def db_for_write(self, model, **hints):
-        # Se realmente for só leitura
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.route_app_labels:
-            return False
+            return db == 'default'
         return None
